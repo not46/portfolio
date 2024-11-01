85e667d9927419a959172c380bdc7a3d2b4bdb89
not46 <132895730+not46@users.noreply.github.com>
2024-11-01 19:09:47 +0530

    Update Work.jsx

--- src/components/home/Work.jsx
+++ src/components/home/Work.jsx
@@ -6,11 +6,11 @@ function Work() {
       <div className="ml-4">
         <h1 className="mt-4  text-2xl font-bold mb-5 ml-10">My Works</h1>
       </div>
-      <div className="flex items-center lg:px-6 justify-center gap-4 p-3 m-2 flex-wrap md:flex-row ">
+      <div className="flex items-center lg:px-6 justify-center gap-4 p-1 m-2 flex-wrap md:flex-row ">
         <div className="ml-2 mt-4 ">
           <iframe
            className="md:w-[60vw] lg:w-[30vw] rounded-lg"
-            width="450"
+            width="370"
             height="300"
             src="https://www.youtube.com/embed/iOdVHqQgA4A?si=d9b5SWs87p4pSRCf"
             title="YouTube video player"
@@ -26,7 +26,7 @@ function Work() {
         <div className="flex  ml-2 mt-4">
           <iframe 
            className="md:w-[60vw] lg:w-[30vw] rounded-lg"
-            width="450"
+            width="370"
             height="300"
             src="https://www.youtube.com/embed/hZZm4XB8DBI?si=FMnWDT43YFsawaI8"
             title="YouTube video player"
