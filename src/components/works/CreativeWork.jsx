fc2df4fa48b9f8a94e4fe0281943dd12d18ca18b
not46 <132895730+not46@users.noreply.github.com>
2024-11-03 00:56:42 +0530

    Update CreativeWork.jsx

--- src/components/works/CreativeWork.jsx
+++ src/components/works/CreativeWork.jsx
@@ -2,114 +2,97 @@ import React from "react";
 
 function CreativeWork() {
   return (
-    <div className="md:flex-row md:flex-wrap md:p-1  md:gap-4 lg:p-4 flex flex-col items-center justify-center gap-3 p-6">
-    
-    
-     
-      <p className="p-1">
+    <div className="grid gap-5 grid-cols-2 p-4 md:gap-4 md:p-4 lg:grid-cols-4 sm:grid-cols-2">
+      <div className="p-1  ">
         <img
-          className="md:w-[40vw] lg:w-[30vw] rounded-xl"
+          className="w-[250px] h-[220px] rounded-xl object-cover"
           src="https://res.cloudinary.com/dgymvrggo/image/upload/v1730460408/Orange_Delhi_Travel_Instagram_Post_glco3f.jpg"
           alt=""
         />
-      </p>
-      <p className="p-1">
+      </div>
+      <div className="p-1">
         <img
-          className="md:w-[40vw] lg:w-[30vw] rounded-xl"
+          className="w-[250px] h-[220px] rounded-xl object-cover"
           src="https://res.cloudinary.com/dgymvrggo/image/upload/v1730460410/Powerful_Engine_vqj0ns.jpg"
           alt=""
         />
-      </p>
-
-      <p className="p-1">
+      </div>
+      <div className="p-1">
         <img
-          className="md:w-[40vw] lg:w-[30vw] rounded-xl"
+          className="w-[250px] h-[220px] rounded-xl object-cover"
           src="https://res.cloudinary.com/dgymvrggo/image/upload/v1730460411/Red_and_Yellow_Illustrated_Biryani_Sale_Instagram_Post_my5hg2.jpg"
           alt=""
         />
-      </p>
-      <p className="p-1">
+      </div>
+      <div className="p-1">
         <img
-          className="md:w-[40vw]  lg:w-[30vw] rounded-xl"
+          className="w-[250px] h-[220px] rounded-xl object-cover"
           src="https://res.cloudinary.com/dgymvrggo/image/upload/v1730460432/SUN-01_nvuuab.jpg"
           alt=""
         />
-      </p>
-      <p className="p-1">
+      </div>
+      <div className="p-1">
         <img
-          className="md:w-[40vw] lg:w-[30vw] rounded-xl"
+          className="w-[250px] h-[220px] rounded-xl object-cover"
           src="https://res.cloudinary.com/dgymvrggo/image/upload/v1730460449/SUN02_fnnrol.png"
           alt=""
         />
-      </p>
-     
-
-      <p className="p-1">
-        <video className="md:w-[40vw] rounded-xl  lg:w-[30vw] " controls>
+      </div>
+      <div className="p-1">
+        <video className="w-[250px] h-[220px] rounded-xl object-cover" controls>
           <source src="https://res.cloudinary.com/dgymvrggo/video/upload/v1730460440/skipper_bihu_005_xd8c3g.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      </p>
-    
-      <p className="p-1">
-        <video className="md:w-[40vw] lg:w-[30vw]  rounded-xl" controls>
+      </div>
+      <div className="p-1">
+        <video className="w-[250px] h-[220px] rounded-xl object-cover" controls>
           <source src="https://res.cloudinary.com/dgymvrggo/video/upload/v1730461781/nishant_baid_7_ggoakv.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      </p>
-      <p className="p-1">
-        <video className="md:w-[40vw] lg:w-[30vw]  rounded-xl" controls>
+      </div>
+      <div className="p-1">
+        <video className="w-[250px] h-[220px] rounded-xl object-cover" controls>
           <source src="https://res.cloudinary.com/dgymvrggo/video/upload/v1730461750/bbq_durga_puja_4_aumbky.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      </p>
-
-      <p className="p-1">
+      </div>
+      <div className="p-1">
         <img
-          className="md:w-[40vw] rounded-xl  lg:w-[30vw]"
+          className="w-[250px] h-[220px] rounded-xl object-cover"
           src="https://res.cloudinary.com/dgymvrggo/image/upload/v1730460406/Black_Red_Simple_Food_Photo_Animated_Video_Your_Story_1080_x_1350_px_q9liax.jpg"
           alt=""
         />
-      </p>
-      <p className="p-1">
-        <video className="md:w-[40vw] lg:w-[30vw]  rounded-xl" controls>
+      </div>
+      <div className="p-1">
+        <video className="w-[250px] h-[220px] rounded-xl object-cover" controls>
           <source src="https://res.cloudinary.com/dgymvrggo/video/upload/v1730461723/Bbq_diwali_3_1_jgnkir.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      </p>
-      <p className="p-1">
-        <video className="md:w-[40vw] lg:w-[30vw]  rounded-xl" controls>
+      </div>
+      <div className="p-1">
+        <video className="w-[250px] h-[220px] rounded-xl object-cover" controls>
           <source src="https://res.cloudinary.com/dgymvrggo/video/upload/v1730461592/Hom_bolly_V4_lzkb5o.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      </p>
-      <p className="p-1">
-        <video className="md:w-[40vw] lg:w-[30vw]  rounded-xl" controls>
+      </div>
+      <div className="p-1">
+        <video className="w-[250px] h-[220px] rounded-xl object-cover" controls>
           <source src="https://res.cloudinary.com/dgymvrggo/video/upload/v1730460490/10octfinal_vauoyz.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      </p>
-      <p className="p-1">
-        <video className="md:w-[40vw] lg:w-[30vw]  rounded-xl" controls>
+      </div>
+      <div className="p-1">
+        <video className="w-[250px] h-[220px] rounded-xl object-cover" controls>
           <source src="https://res.cloudinary.com/dgymvrggo/video/upload/v1730460460/Hlw_1_hch6la.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      </p>
-      <p className="p-1">
-        <video className="md:w-[40vw] lg:w-[30vw]  rounded-xl" controls>
+      </div>
+      <div className="p-1">
+        <video className="w-[250px] h-[220px] rounded-xl object-cover" controls>
           <source src="https://res.cloudinary.com/dgymvrggo/video/upload/v1730460484/MS01062024_sxdwer.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      </p>
-
-
-
-
-
-
-
-
-
+      </div>
     </div>
   );
 }
