552f4a2ca849bd7ab7aa345f7204c758ddd1951d
not46 <132895730+not46@users.noreply.github.com>
2024-11-03 00:54:58 +0530

    Update Skills.jsx

--- src/components/home/Skills.jsx
+++ src/components/home/Skills.jsx
@@ -1,15 +1,12 @@
-
-
-import React from 'react';
+import React from "react";
 
 const Skills = () => {
   return (
     <section className="relative p-8 lg:pt-5 lg:pl-1 lg:pb-6 ">
-        
       <div className="container mx-auto">
-      <h1 className='text-2xl  ml-3 lg:ml-4 font-bold mb-4'>
-        What I Offer
-       </h1>
+        <h1 className="text-2xl  ml-3 lg:ml-4  mb-4">What I  <span className="font-bold">Offer</span></h1>
+       
+       
         <div className="grid gap-4 lg:gap-8 grid-cols-12 ">
           {/* First Block - Large Image */}
           <div className="col-span-12 lg:col-span-5 row-span-8 flex justify-center rounded-lg overflow-hidden">
@@ -25,7 +22,8 @@ const Skills = () => {
           <div
             className="col-span-12 lg:col-span-4 row-span-4 flex flex-col justify-end items-start p-5 bg-cover bg-center bg-no-repeat rounded-lg relative"
             style={{
-              backgroundImage: "url('https://res.cloudinary.com/dgymvrggo/image/upload/v1730465772/videoEdit_jmomlw.jpg')",
+              backgroundImage:
+                "url('https://res.cloudinary.com/dgymvrggo/image/upload/v1730465772/videoEdit_jmomlw.jpg')",
             }}
           >
             <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg"></div>
@@ -38,7 +36,8 @@ const Skills = () => {
           <div
             className="col-span-12 lg:col-span-3 row-span-4 flex flex-col justify-end items-start p-5 bg-cover bg-center bg-no-repeat rounded-lg relative"
             style={{
-              backgroundImage: "url('https://res.cloudinary.com/dgymvrggo/image/upload/v1730465772/brand_raerew.jpg')",
+              backgroundImage:
+                "url('https://res.cloudinary.com/dgymvrggo/image/upload/v1730465772/brand_raerew.jpg')",
             }}
           >
             <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg"></div>
@@ -51,7 +50,8 @@ const Skills = () => {
           <div
             className="col-span-12 lg:col-span-3 row-span-4 flex flex-col justify-end items-start p-5 bg-cover bg-center bg-no-repeat rounded-lg relative"
             style={{
-              backgroundImage: "url('https://res.cloudinary.com/dgymvrggo/image/upload/v1730460783/OVERDUE-2_nw98su.jpg')",
+              backgroundImage:
+                "url('https://res.cloudinary.com/dgymvrggo/image/upload/v1730460783/OVERDUE-2_nw98su.jpg')",
             }}
           >
             <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg"></div>
@@ -64,11 +64,14 @@ const Skills = () => {
           <div
             className="col-span-12 lg:col-span-4 row-span-4 flex flex-col justify-end items-start p-5 bg-cover bg-center bg-no-repeat rounded-lg relative"
             style={{
-              backgroundImage: "url('https://res.cloudinary.com/dgymvrggo/image/upload/v1730461328/Ipl_2_m5a1dy.jpg')",
+              backgroundImage:
+                "url('https://res.cloudinary.com/dgymvrggo/image/upload/v1730461328/Ipl_2_m5a1dy.jpg')",
             }}
           >
             <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg"></div>
-            <h3 className="relative z-10 text-3xl font-mono text-gray-300">Thumbnails</h3>
+            <h3 className="relative z-10 text-3xl font-mono text-gray-300">
+              Thumbnails
+            </h3>
           </div>
         </div>
       </div>
