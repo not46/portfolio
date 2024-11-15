54cf15d708357d44281c259ca708ba9093ec879a
not46 <132895730+not46@users.noreply.github.com>
2024-11-15 19:12:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,36 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
-import Footer from './components/Footer';
-import About from './components/About';
-import Navbar from './components/home/Navbar';
-import ContactForm from './components/ContactForm';
-import Work from './components/Work';
 import React from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Navbar from "./components/home/Navbar";
+import ContactForm from "./components/ContactForm";
+import Work from "./components/Work";
 
 function App() {
-  // Set the theme to dark mode directly
   const darkTheme = createTheme({
     palette: {
-      mode: 'dark',
+      mode: "dark",
     },
   });
 
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <div className="mb-10">
+      <div className="absolute top-0 left-0 w-full z-10">
         <Navbar />
       </div>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/work" element={<Work />} />
         <Route path="/contact" element={<ContactForm />} />
       </Routes>
-
       <Footer />
     </ThemeProvider>
   );
 }
 
-export default App;
+export default App; // Ensure App is exported as default
